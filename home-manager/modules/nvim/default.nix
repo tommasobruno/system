b66d399f2fb94b0968dc4d9cfd31fea35389df20
tommasobruno <bruno.tommaso@protonmail.com>
2024-07-11 15:42:36 +0100

    feat: improve oil configuration by hiding folders/files

--- home-manager/modules/nvim/default.nix
+++ home-manager/modules/nvim/default.nix
@@ -25,7 +25,19 @@ in {
     };
 
     plugins = {
-      oil.enable = true;
+      oil = {
+        enable = true;
+        settings = {
+          view_options = {
+            show_hidden = true;
+            is_always_hidden = ''
+              function(name, _)
+                return name == '..' or name == '.git' or name == '.DS_Store'
+              end
+            '';
+          };
+        };
+      };
 
       telescope = {
         enable = true;
