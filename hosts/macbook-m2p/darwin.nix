2644e05cc840c9df184ba4106a5191f667874ab5
tommasobruno <bruno.tommaso@protonmail.com>
2024-07-11 15:27:24 +0100

    feat: add obsidian in dock apps

--- hosts/macbook-m2p/darwin.nix
+++ hosts/macbook-m2p/darwin.nix
@@ -3,6 +3,7 @@
   dock.apps = [
     "/Applications/Arc.app"
     "/Users/tommasobruno/Applications/Home Manager Apps/WezTerm.app/"
+    "/Users/tommasobruno/Applications/Home Manager Apps/Obsidian.app/"
   ];
 
 }
