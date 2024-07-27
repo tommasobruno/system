f0c7b4131474136b4d8e973a18c319716512d217
tommasobruno <bruno.tommaso@protonmail.com>
2024-07-27 02:22:20 +0100

    chore: remove nvim

--- core/globals.nix
+++
@@ -1,8 +0,0 @@
-{ ... }: {
-  programs.nixvim.globals = {
-    mapleader = " ";
-    maplocalleader = " ";
-    loaded_netrw = 1;
-    loaded_netrwPlugin = 1;
-  };
-}
--- plugins/snippets/default.nix
+++
@@ -1,1 +0,0 @@
-{ ... }: { imports = [ ./zig.nix ]; }
